515eab69727c667a7575b1cc9b0d061f37952c50
Carrasco Romel <romelx23@gmail.com>
2021-02-17 23:17:10 -0500

    FormContact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 function App() {
   return (
     <div className="container">
       <Header />
+      <Footer/>
     </div>
   );
 }
