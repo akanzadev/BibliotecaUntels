3456365c24ec8c3a0835689a87a7cd104e6de914
JeanDev <jeancarlosmamanilingan@gmail.com>
2021-02-17 22:46:30 -0500

    semi flex

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,14 +11,19 @@ const Home = () => {
             <span>LANDING PAGE DESGIN</span>
           </h1>
           <p>
-            LANDING LANDING LANDING LANDING LANDING LANDING LANDING LANDING
-            LANDING LANDING LANDING LANDING LANDING LANDING LANDING LANDING
+            Si quieres explorar nuevos mundos, historias, personajes únete a la
+            magia de las bibliotecas. Para viajar no necesariamente requieres de
+            un avión con un buen libro ya tienes, y si ese libro lo lees en una
+            biblioteca pues viajas en primera clase.
           </p>
           <button type="button">JOIN NOW</button>
         </div>
         <div className="home-img">
           <figure className="home-img__item">
-            <img src="./assets/Online_education_SVG.svg" alt="Educacion Online" />
+            <img
+              src="./assets/Online_education_SVG.svg"
+              alt="Educacion Online"
+            />
           </figure>
           <figure className="home-img__item">
             <img src="./assets/Books_SVG.svg" alt="Libros Electronicos" />
