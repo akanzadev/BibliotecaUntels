0f30845001917dd84ab630614300a672fc4b15d0
JeanDev <jeancarlosmamanilingan@gmail.com>
2021-02-18 00:31:47 -0500

    ready header

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,31 +1,48 @@
 import { Fragment } from "react";
+import { useState } from "react";
 import "./Nav.css";
 const Nav = () => {
+  const [show, setshow] = useState(false);
+  const showMenu = () => {
+    setshow(!show);
+  };
+
   return (
     <Fragment>
-      <nav className="nav">
-        <figure className="nav-logo">
-          <img
-            src="./assets/logo-untels.png"
-            alt="Biblioteca virtual Untels"
-            title="biblioteca"
-          />
-        </figure>
-        <div className="nav-hamburger">
-          <i class="fas fa-bars"></i>
-        </div>
-        <ul className="nav-list">
-          <li className="nav-list__item">
-            <a href="#">Home</a>
-          </li>
-          <li className="nav-list__item">
-            <a href="#">Search</a>
-          </li>
-          <li className="nav-list__item">
-            <a href="#">About</a>
-          </li>
-        </ul>
-      </nav>
+      <div id="nav">
+        <nav className="nav">
+          <figure className="nav-logo">
+            <img
+              src="./assets/logo-untels.png"
+              alt="Biblioteca virtual Untels"
+              title="biblioteca"
+            />
+          </figure>
+          <div className="nav-hamburger">
+            <i
+              className={show ? `fas fa-times` : `fas fa-bars`}
+              onClick={showMenu}
+            ></i>
+          </div>
+          <ul className={show ? `nav-list show` : `nav-list`}>
+            <li className="nav-list__item">
+              <a href="#">
+                <i className="fas fa-home"></i>Home
+              </a>
+            </li>
+            <li className="nav-list__item">
+              <a href="#">
+                <i className="fas fa-book"></i>Search
+              </a>
+            </li>
+            <li className="nav-list__item">
+              <a href="#">
+                <i className="fas fa-address-card"></i>About
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </Fragment>
   );
 };
