46a1d8d130b9f8859ce43648a15423dd2e3880bd
Jeancarlos Mamani Lingan <jeancarlosmamanilingan@gmail.com>
2021-02-17 20:21:03 -0500

    c

---
+++ src/c/Header/Header.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const Header = () => {
+  return (
+      <Fragment>
+      </Fragment>
+  );
+};
+
+
+export default Header;
---
+++ src/c/Home/Home.js
@@ -0,0 +1,34 @@
+import { Fragment } from "react";
+
+import "./Home.css";
+const Home = () => {
+  return (
+    <Fragment>
+      <div className="home">
+        <div className="home-text">
+          <h1>
+            ONLINE<strong>LIBRARY</strong>
+            <span>LANDING PAGE DESGIN</span>
+          </h1>
+          <p>
+            LANDING LANDING LANDING LANDING LANDING LANDING LANDING LANDING
+            LANDING LANDING LANDING LANDING LANDING LANDING LANDING LANDING
+            LANDING LANDING LANDING LANDING LANDING LANDING LANDING LANDING
+            LANDING LANDING LANDING LANDING LANDING LANDING
+          </p>
+          <button type="button">JOIN NOW</button>
+        </div>
+        <div className="home-img">
+          <figure className="home-img__item">
+            <img src="./assets/Online_education_SVG.svg" alt="Educacion Online" />
+          </figure>
+          <figure className="home-img__item">
+            <img src="./assets/Books_SVG.svg" alt="Libros Electronicos" />
+          </figure>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Home;
